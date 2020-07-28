aaa5a8b23c7b9bff2b0d4b6aeb6c953ba7c5370f
Bethaly <bethaly.tenango@gmail.com>
2020-07-28 01:16:09 -0600

    Update1

--- makeit/front-end/src/App.vue
+++ makeit/front-end/src/App.vue
@@ -1,11 +1,42 @@
 <template>
   <div id="app">
+     <b-navbar toggleable="lg" type="dark" variant="info">
+    
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to= "/" >Home</b-nav-item>
+        <b-nav-item href="/about" >Create</b-nav-item>
+
+        <b-nav-item-dropdown text="Categories" right>
+          <b-dropdown-item href="#">Games</b-dropdown-item>
+          <b-dropdown-item href="#">Anime</b-dropdown-item>
+          <b-dropdown-item href="#">Series</b-dropdown-item>
+          <b-dropdown-item href="#">Movies</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+<!--Center -->
+<div>
+<b-navbar-brand class="title" href="#">What have you watched?</b-navbar-brand>
+</div>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    
   </div>
+  
 </template>
 
 <style>
--- makeit/front-end/src/views/Home.vue
+++ makeit/front-end/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>This Web will help you to tack all your TV Series, Games, Anime that you have watched</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
